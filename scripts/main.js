f05edbc59568af025adca0ace250c30bbeba8494
Zachary Portillo <portillozachary@gmail.com>
2019-03-05 21:33:19 -0800

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+import Typed from './scripts/typed.js';
+console.log('hello')
+      var options = {
+        strings: ['hello','there','adsf','fdsd','advac'],
+        typeSpeed: 40
+      }
+    
+      var typed = new Typed("#typed", options);
