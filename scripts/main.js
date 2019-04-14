3fc7e23f0ce777fc8b101dabd074533606a9ce38
Zachary Portillo <portillozachary@gmail.com>
2019-04-13 20:48:06 -0700

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,2 @@
-import Typed from './scripts/typed.js';
-console.log('hello')
-      var options = {
-        strings: ['hello','there','adsf','fdsd','advac'],
-        typeSpeed: 40
-      }
-    
-      var typed = new Typed("#typed", options);
+const submit = document.getElementById("submit")
+ 
